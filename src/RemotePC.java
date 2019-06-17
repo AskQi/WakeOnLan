bcf7647596d92434d7c56f310bc9c36babd209c4
Yvan Stemmerik <y.stemmerik@student.vu.nl>
2019-06-17 15:40:26 +0200

    Create small readme

--- src/RemotePC.java
+++ src/RemotePC.java
@@ -1,6 +1,8 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.Executors;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import static java.lang.Integer.parseInt;
 
 public class RemotePC {
     private String netwerknaam;
@@ -48,5 +50,48 @@ public class RemotePC {
             Main.sendCommand(command);
         }
     }
+
+    void wakeOnLan(String ipStr, String macStr){
+        final int PORT = 9;
+        try {
+            byte[] macBytes = getMacBytes(macStr);
+            byte[] bytes = new byte[6 + 16 * macBytes.length];
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) 0xff;
+            }
+            for (int i = 6; i < bytes.length; i += macBytes.length) {
+                System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+            }
+
+            InetAddress address = InetAddress.getByName(ipStr);
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, PORT);
+            DatagramSocket socket = new DatagramSocket();
+            socket.send(packet);
+            socket.close();
+            System.out.println("Wake-on-LAN packet sent.");
+        }
+        catch (Exception e) {
+            System.out.println("Failed to send Wake-on-LAN packet:" + e);
+            System.exit(1);
+        }
+    }
+
+    private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
+        byte[] bytes = new byte[6];
+        String[] hex = macStr.split("(\\:|\\-)");
+        if (hex.length != 6) {
+            throw new IllegalArgumentException("Invalid MAC address.");
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) parseInt(hex[i], 16);
+            }
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid hex digit in MAC address.");
+        }
+        return bytes;
+    }
+
 }
 
